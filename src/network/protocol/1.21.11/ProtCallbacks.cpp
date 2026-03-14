1e8f21ee4392dd0b9b5f74ba61315cb2c340e3c4
ncc1700 <nearestcommit@gmail.com>
2026-03-13 20:57:31 -0500

    more stubs

--- src/network/protocol/1.21.11/ProtCallbacks.cpp
+++ src/network/protocol/1.21.11/ProtCallbacks.cpp
@@ -16,18 +16,19 @@ namespace Network {
         static std::atomic_bool loginSuccess = false;
 
         static int8_t loginSuccessCallback(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            if(loginSuccess == true){
+                // why are we sending this again?
+                printf("ERROR: tried sending loginSuccess when its useless!\n");
+                return -1;
+            }
             printf("Server acknowledged our login\n");
             loginAcknowledged(con.get());
+            loginSuccess = true;
             return 0;
         }
 
         static int8_t serverKeepAlive(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
             printf("Server wants to check up on us\n");
-            // int id = packet->readU64();
-            // Packet p;
-            // p.writeVarInt(0x04);
-            // p.writeU64(id);
-            // PacketSender::send(std::make_shared<Packet>(p));
             PacketSender::send(packet);
             return 0;
         }
@@ -44,12 +45,122 @@ namespace Network {
         int8_t isLoginSuccess(){
             return loginSuccess;
         }
-        void registerCallbacks(){
-            callbacks[0x2] = loginSuccessCallback;
-            callbacks[0x4] = serverKeepAlive;
-            callbacks[0xe] = knownPacks;
-            callbacks[0x3] = finishConfig;
+        static int8_t changeDifficulty(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s", __FUNCTION__);
+            // make server happy
+            int difficulty = packet->readByte();
+            Packet sentPacket;
+            sentPacket.writeVarInt(0x03);
+            sentPacket.writeByte(difficulty);
+            PacketSender::send(std::make_shared<Packet>(sentPacket));
+            return 0;
         }
+        static int8_t playerAbilites(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t setHeldItem(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t updateRecipes(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t entityEvent(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t commandsListed(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t syncPlayerPos(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("Syncing player position\n");
+            // do some syncing here artdev/jojo.....
+            printf("synced, sending confirmation to server\n");
+            int teleportId = packet->readVarInt();
+            Packet sentPacket;
+            sentPacket.writeVarInt(0x00);
+            sentPacket.writeVarInt(teleportId);
+            PacketSender::send(std::make_shared<Packet>(sentPacket));
+            // you can then do another packet if yall want with extra info
+            // https://minecraft.wiki/w/Java_Edition_protocol/Packets#Set_Player_Position_and_Rotation
+            // ill just skip it for times sake
+            return 0;
+        }
+        static int8_t serverData(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t playerInfoUpdate(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t initWorldBorder(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t updateTime(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t setDefaultSpawnPos(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t gameEvent(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t setTickingState(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t stepTick(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t setCenterChunk(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("STUB: %s\n", __FUNCTION__);
+            return 0;
+        }
+        static int8_t chunkDataAndUpdateLight(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet){
+            printf("for artdev, chunkdata and light\n");
+            // ok heres ya data, you can see whats sent via
+            // https://minecraft.wiki/w/Java_Edition_protocol/Packets#Chunk_Data_and_Update_Light
+
+            // finally we send the final packet of the basic initialization
+            // of the minecraft JE client
+            Packet loadedPacket;
+            loadedPacket.writeVarInt(0x2B);
+            PacketSender::send(std::make_shared<Packet>(loadedPacket));
+            return 0;
+        }
+        void registerCallbacks(){
+            // very messy =(
+            callbacks[0x02] = loginSuccessCallback;
+            callbacks[0x4] = serverKeepAlive;
+            callbacks[0xE] = knownPacks;
+            callbacks[0x3] = finishConfig;
+            callbacks[0x0A] = changeDifficulty;
+            callbacks[0x3E] = playerAbilites;
+            callbacks[0x67] = setHeldItem; // SIX SEVEEEEEN
+            callbacks[0x83] = updateRecipes;
+            callbacks[0x22] = entityEvent;
+            callbacks[0x10] = commandsListed;
+            callbacks[0x46] = syncPlayerPos;
+            callbacks[0x54] = serverData;
+            callbacks[0x44] = playerInfoUpdate;
+            callbacks[0x2A] = initWorldBorder;
+            callbacks[0x6F] = updateTime;
+            callbacks[0x5F] = setDefaultSpawnPos;
+            callbacks[0x26] = gameEvent;
+            callbacks[0x7D] = setTickingState;
+            callbacks[0x7E] = stepTick;
+            callbacks[0x5C] = setCenterChunk;
+            callbacks[0x2C] = chunkDataAndUpdateLight;
+        }   
 
         int8_t handleCallback(std::shared_ptr<Connection> con, std::shared_ptr<Packet> packet, int id){
             if(callbacks.count(id) == 0){
